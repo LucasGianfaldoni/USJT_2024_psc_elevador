89ae48fff7ef48afb39e22c2cff307edb0118b35
824139513 <824139513@PTALI20327.animaedu.intranet>
2024-04-15 21:39:25 -0300

    Versão inicial

---
+++ src/main/java/com/mycompany/elvador/ElevadorMain.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.elvador;
+
+/**
+ *
+ * @author 824139513
+ */
+public class ElevadorMain {
+    public static void main(String[]args){
+        TelaeElevador tela = new TelaeElevador();
+        tela.setVisible(true);
+    } 
+}
